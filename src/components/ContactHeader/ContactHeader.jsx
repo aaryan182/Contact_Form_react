69c4f226b09a662512bf9c0f87cc1d975a23ae5d
Aaryan Bajaj <aaryanbajaj385@gmail.com>
2023-08-20 16:15:04 +0530

    first commit

---
+++ src/components/ContactHeader/ContactHeader.jsx
@@ -0,0 +1,12 @@
+import styles from "./ContactHeader.module.css";
+
+const ContactHeader = () => {
+  return (
+    <div className={`${styles.contact_section}`}>
+        <h1>CONTACT US</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ex ducimus dolore dolorem consequatur molestias neque aliquid sint voluptatum laboriosam.</p>
+    </div>
+  )
+}
+
+export default ContactHeader
